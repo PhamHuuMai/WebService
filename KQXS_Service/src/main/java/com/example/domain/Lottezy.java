629da64f285cc036c7f74cdc30df8ad49dcd09ba
PhamHuuMai <huumaipham1996@gmail.com>
2017-10-10 23:43:19 +0700

    skeleton

---
+++ KQXS_Service/src/main/java/com/example/domain/Lottezy.java
@@ -0,0 +1,40 @@
+package com.example.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "lottezy")
+public class Lottezy {
+	@Id
+	private String id;
+	@Field("name")
+	private String name;
+	@Field("address")
+	private String address;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
---
+++ KQXS_Service/src/main/java/com/example/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "user")
+public class User {
+	@Id
+	private String id;
+	@Field("account")
+	private String account;
+	@Field("password")
+	private String password;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
